19e59ecd85bf39bea1575333a088736ffa354a96
muslih <muslihmuhammed06@gmail.com>
2023-03-27 12:44:24 +0530

    after clearing cache

--- config/collections.js
+++
@@ -1,9 +0,0 @@
-module.exports={
-user_collection :"userData",
-admin_collection :"adminData",
-product_collection :"product",
-category_collection :"category",
-brand_collection :"brand",
-cart_collection :"cart"
-
-}
