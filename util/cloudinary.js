802dbe7657399fcd86f1003bdf5cba495a04c7c7
muslih <muslihmuhammed06@gmail.com>
2023-04-26 11:33:19 +0530

    change cluodinary

--- util/cloudinary.js
+++ util/cloudinary.js
@@ -3,6 +3,6 @@ const dotenv = require('dotenv').config();
 cloudinary.config({
     cloud_name:process.env.CLOUD_NAME,
     api_key:process.env.API_KEY ||'586789217435372',
-    api_secret:process.env.API_SECRET 
+    api_secret:process.env.API_SECRET ||'ZRJn0FitZtDBg7c7PF7jkb8wyos'
   });
 module.exports=cloudinary
