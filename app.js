ff1db939c8a5ceba34a0b1f19a5a23dd68b48a55
muslih <muslihmuhammed06@gmail.com>
2023-04-26 20:41:58 +0530

    commented code product details

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   // render the error page
   res.status(err.status || 500);
-  res.render('error',err);  
+  res.render('error');  
 });
 
 const PORT = process.env.PORT || 8080;
