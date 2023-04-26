1485a79cbc7541d164f5c7a28880692a63215b35
muslih <muslihmuhammed06@gmail.com>
2023-04-26 11:40:49 +0530

    cloud name change

--- util/cloudinary.js
+++ util/cloudinary.js
@@ -1,7 +1,7 @@
 const cloudinary = require('cloudinary').v2
 const dotenv = require('dotenv').config();
 cloudinary.config({
-    cloud_name:process.env.CLOUD_NAME,
+    cloud_name:process.env.CLOUD_NAME || 'dhk0hs5b0',
     api_key:process.env.API_KEY ||'586789217435372',
     api_secret:process.env.API_SECRET ||'ZRJn0FitZtDBg7c7PF7jkb8wyos'
   });
